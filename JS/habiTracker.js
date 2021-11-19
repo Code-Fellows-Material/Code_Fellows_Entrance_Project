01b751ba1de43afe161411dd99783398fb660847
Kellen-Linse <kblinse06@gmail.com>
2021-11-19 00:07:15 -0800

    Add habit creation logic

--- JS/habiTracker.js
+++ JS/habiTracker.js
@@ -1,10 +1,40 @@
 const monthCell = document.querySelector('.month-cell');
+fillMonth(monthCell);
 
-
-for(let i = 0; i < 31; i++){
+function fillMonth(cell){
+    for(let i = 0; i < 31; i++){
     const newDiv = document.createElement('div');
     newDiv.classList.add('cell');
     newDiv.classList.add('month-day-cell');
     newDiv.innerText = i+1;
-    monthCell.appendChild(newDiv);
+    cell.appendChild(newDiv);
+    }
+}
+
+// Event listener for habit input form
+const form = document.querySelector('#habit-input-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); // This will stop the form from navigating to the new page on submission.
+    const input = document.querySelector('#habit-input');
+    createNewHabit(input.value);
+    createNewHabitGrid();
+    input.value = "";
+})
+
+function createNewHabit(habit){
+    console.log(habit);
+    const habitCell = document.querySelector('.habit-cell');
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('cell');
+    newDiv.innerText = habit;
+    habitCell.appendChild(newDiv);
+}
+
+function createNewHabitGrid(){
+    const habitCell = document.querySelector('.tracking-cell');
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('month-cell');
+    habitCell.appendChild(newDiv);
+    fillMonth(newDiv);
 }
