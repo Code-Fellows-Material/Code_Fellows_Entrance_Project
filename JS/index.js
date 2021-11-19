0791a2c408f73820c39e3db3da66bc6b4b1be983
Kellen-Linse <kblinse06@gmail.com>
2021-11-19 05:01:06 -0800

    Add username add password check logic

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,12 @@
 //Variables and Classes
 
 let userArray = [];
+let currentUser = {};
 if(localStorage.userArray){
     userArray = JSON.parse(localStorage.userArray);
 }
 console.log(`Current number of registered users: ${userArray.length}`);
+console.log(userArray);
 
 class userProfile {
     constructor(username, password) {
@@ -15,33 +17,110 @@ class userProfile {
 }
 
   // Event listener for habit input form
-const form = document.querySelector('#signup-form');
+const signUpForm = document.querySelector('#signUpButton');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); // This will stop the form from navigating to the new page on submission.
-    userArray.push(createNewUser());
+signUpForm.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    console.log("sign up event"); 
+    const newUser = createNewUser();
+    newUser && userArray.push(newUser);
     localStorage.setItem('userArray', JSON.stringify(userArray));
-    // console.log(userArray);
-})
+    
+});
+
+const loginForm = document.querySelector('#loginButton');
+
+loginForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("log in event"); 
+    loginUser();
+});
 
 function createNewUser(){
     const userInput = document.querySelector('#sign-up-user');
     const passInput = document.querySelector('#sign-up-pass');
-    userCheck(userInput.value);
-    const newUser = new userProfile(userInput.value, passInput.value);
-    userInput.value = "";
-    passInput.value = "";
-    return newUser;
+    if(userExistenceCheck(userInput.value)){
+        const label = document.querySelector('#sign-up-label');
+        const oldP = document.querySelector('.warning');
+        oldP && oldP.remove();
+        const newP = document.createElement('p');
+        newP.classList.add('warning');
+        newP.innerText = `The username \"${userInput.value}\" already exists!`;
+        label.insertAdjacentElement('afterend', newP);
+        userInput.value = "";
+        passInput.value = "";
+        return false;
+    } else {
+        const newUser = new userProfile(userInput.value, passInput.value);
+        userInput.value = "";
+        passInput.value = "";
+        return newUser;
+    }
 }
 
-function userCheck(userInput){
+function userExistenceCheck(userName){
+    for(let user of userArray){
+        if(user.username != null){
+            console.log(`compare:${user.username} to ${userName}`);
+            if(user.username === userName){
+                console.log('username already exists!');
+                return true;
+            }
+        }
+    }
+    console.log('username does not exist!');
+    return false;
+}
+
+
+
+function loginUser(){
+    const userInput = document.querySelector('#login-user');
+    const passInput = document.querySelector('#login-pass');
+    if(!userExistenceCheck(userInput.value)){
+        const label = document.querySelector('#login-label');
+        const oldP = document.querySelector('.warning');
+        oldP && oldP.remove();
+        const newP = document.createElement('p');
+        newP.classList.add('warning');
+        newP.innerText = `The username \"${userInput.value}\" does not exists!`;
+        label.insertAdjacentElement('afterend', newP);
+        console.log('herein');
+    } else if (passCheck(indexOfUserCheck(userInput.value), passInput.value)){
+        console.log(indexOfUserCheck(userInput.value));
+        console.log('checks out');
+    } else {
+        const label = document.querySelector('#login-label');
+        const oldP = document.querySelector('.warning');
+        oldP && oldP.remove();
+        const newP = document.createElement('p');
+        newP.classList.add('warning');
+        newP.innerText = `username or password is incorrect.`;
+        label.insertAdjacentElement('afterend', newP);
+    }
+}
+
+
+
+
+function indexOfUserCheck(userInput){
+    userCount = 0;
     for(let user of userArray){
         console.log(`${user.username} vs ${userInput}`)
         if(user.username === userInput){
-            console.log('same username already exists!');
-            return false;
+            console.log(`username exists at index ${userCount}`);
+            return userCount;
         }
-        console.log('we good');
+        userCount++;
     }
-    return true;
+}
+
+function passCheck(index, passInput){
+    console.log(`compare: ${userArray[index].password} to ${passInput}`);
+        if(userArray[index].password === passInput){
+            console.log('password matches');
+            return true;
+        }
+        console.log('password does not match');
+    return false;
 }
