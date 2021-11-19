eeed043da2d00a8f8d9c108793de55ca86621741
Kellen-Linse <kblinse06@gmail.com>
2021-11-19 02:18:30 -0800

    Add username check logic.

--- JS/index.js
+++ JS/index.js
@@ -4,7 +4,7 @@ let userArray = [];
 if(localStorage.userArray){
     userArray = JSON.parse(localStorage.userArray);
 }
-// console.log(userArray);
+console.log(`Current number of registered users: ${userArray.length}`);
 
 class userProfile {
     constructor(username, password) {
@@ -21,14 +21,27 @@ form.addEventListener('submit', (e) => {
     e.preventDefault(); // This will stop the form from navigating to the new page on submission.
     userArray.push(createNewUser());
     localStorage.setItem('userArray', JSON.stringify(userArray));
-    console.log(userArray);
+    // console.log(userArray);
 })
 
 function createNewUser(){
     const userInput = document.querySelector('#sign-up-user');
     const passInput = document.querySelector('#sign-up-pass');
+    userCheck(userInput.value);
     const newUser = new userProfile(userInput.value, passInput.value);
     userInput.value = "";
     passInput.value = "";
     return newUser;
+}
+
+function userCheck(userInput){
+    for(let user of userArray){
+        console.log(`${user.username} vs ${userInput}`)
+        if(user.username === userInput){
+            console.log('same username already exists!');
+            return false;
+        }
+        console.log('we good');
+    }
+    return true;
 }
